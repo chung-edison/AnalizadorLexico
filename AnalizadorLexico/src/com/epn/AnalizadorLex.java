874a8ec2a8ad144200203601cb077c7f6a2f1f33
chung-edison <eddy2809@hotmail.com>
2015-11-23 16:07:43 -0500

    AL 1.0
    Separa tokens con espacios antes de evaluar y clasificar.
    Cambiado orden
    de las comparaciones, ahora evalúa string y char primero.
    .jar
    ejecutable creado.

---
+++ AnalizadorLexico/src/com/epn/AnalizadorLex.java
@@ -0,0 +1,59 @@
+package com.epn;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AnalizadorLex {
+
+	private static int linea;
+
+	public static void main(String[] args) throws IOException {
+
+		ComparadorLex comparador = new ComparadorLex();
+
+		BufferedReader input = null;
+		BufferedWriter output = null;
+
+		linea = 0;
+
+		try {
+
+			File inputFile = new File("input.txt");
+
+			input = new BufferedReader(new FileReader(inputFile));
+
+			File outputFile = new File("output.csv");
+
+			output = new BufferedWriter(new FileWriter(outputFile));
+
+			while (input.ready()) {
+				output.write(comparador.analizarLinea(input.readLine()));
+				linea++;
+				if (comparador.hayError()) {
+					System.out.println("Error lexico en la linea " + linea);
+					comparador.setError(false);
+				}
+			}
+
+			System.out.println("Lineas analizadas: " + linea);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (input != null)
+					input.close();
+				if (output != null)
+					output.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+}
---
+++ AnalizadorLexico/src/com/epn/ComparadorLex.java
@@ -0,0 +1,70 @@
+package com.epn;
+
+public class ComparadorLex {
+	
+	//diccionario de palabras reservadas
+	private static final String reservadas = "int|float|bool|char|string|if|then|else|while|do|input|output|return"; 
+
+	//diccionario de caracteres especiales
+	private static final String especiales = ",|;|:|\\(|\\)|\\[|\\]|\\{|\\}|\\+|-|\\*|/|<|>|=|!|&|\\$";
+
+	//diccionario de operadores compuestos
+	private static final String compuestos = "<=|>=|==|!=|&&|\\|\\|";
+	
+	private static final String nosignos = ",|;|:|\\(|\\)|\\[|\\]|\\{|\\}|/|\\$";
+	
+	private static final String texto = "(?:\".*\")|([0-9]|[a-z]|[A-Z]|_)+";
+	
+	private static final String numeros = "(-)?[0-9]+((\\.)[0-9]+)?";
+
+	private boolean error;  //bandera de error para determinar la linea donde ocurrio el error
+	
+	private String n = System.getProperty("line.separator"); //obtiene el separador de linea del sistema
+	
+	public boolean hayError() {
+		return error;
+	}
+
+	public void setError(boolean error) {
+		this.error = error;
+	}
+
+	public String analizarLinea(String line){
+		line = line.replaceAll(numeros + "|" + texto + "|" + nosignos, " $0 ").trim(); 
+			//agrega espacios antes de cada id y num, es decir, identifica tokens y separa
+		
+		//System.out.println(line); //muestra la linea luego de haber añadido espacios
+		String[] tokens = line.split("\\s+"); //separa el string en varios strings segun los espacios
+		
+		String result = "";
+		for(String token : tokens){
+			result += "\"" + token + "\"" + "," + tokenClass(token) + n; //formato de CSV
+		}
+		return result;
+	}
+	
+	public String tokenClass(String token) {
+		if (token.matches("\".*\"")) { // string
+			return "string";
+		} else if (token.matches("\'.\'")) { // char
+			return "char";
+		} else if (token.matches(reservadas)) { // palabras reservadas
+			return "palabra reservada";
+		} else if (token.matches("(true|false)")) { // bool
+			return "bool";
+		} else if (token.matches(especiales)) { // caracteres especiales
+			return "caracter especial";
+		} else if (token.matches(compuestos)) { // operadores compuestos
+			return "operador compuesto";
+		} else if (token.matches("[a-z]([0-9]|[a-z]|[A-Z]|_)*")) { // identificadores
+			return "identificador";
+		} else if (token.matches("(-)?[0-9]+(\\.)[0-9]+")) { // float
+			return "float";
+		} else if (token.matches("(-)?[0-9]+")) { // int
+			return "int";
+		}
+		error = true;
+		return "errorlex";
+	}
+
+}
