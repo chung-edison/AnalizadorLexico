fbe2d9c5b1f42062154c7d26c4c59fe822c29004
chung-edison <eddy2809@hotmail.com>
2015-11-23 22:27:13 -0500

    AL 1.1
    Ahora ignora string de la forma "string string".
    Ahora ignora comentarios multilinea.
    Cambiado el orden de las comparaciones.

--- AnalizadorLexico/src/com/epn/ComparadorLex.java
+++ AnalizadorLexico/src/com/epn/ComparadorLex.java
@@ -19,6 +19,8 @@ public class ComparadorLex {
 
 	private boolean error;  //bandera de error para determinar la linea donde ocurrio el error
 	
+	private boolean comment;	//bandera para ignorar comentarios
+	
 	private String n = System.getProperty("line.separator"); //obtiene el separador de linea del sistema
 	
 	public boolean hayError() {
@@ -30,11 +32,18 @@ public class ComparadorLex {
 	}
 
 	public String analizarLinea(String line){
+		if(line.matches(".*\\*/")) {
+			comment = false;
+			return "";
+		}
+		if(line.matches("//.*")||comment) return "";
+		if(line.matches("/\\*.*")) comment = true;
 		line = line.replaceAll(numeros + "|" + texto + "|" + nosignos, " $0 ").trim(); 
 			//agrega espacios antes de cada id y num, es decir, identifica tokens y separa
 		
 		//System.out.println(line); //muestra la linea luego de haber añadido espacios
-		String[] tokens = line.split("\\s+"); //separa el string en varios strings segun los espacios
+		String[] tokens = line.split("\\s+(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); //separa el string en varios strings segun los espacios
+																			//toma en cuenta que haya 0 o un numero par de comillas
 		
 		String result = "";
 		for(String token : tokens){
@@ -52,10 +61,10 @@ public class ComparadorLex {
 			return "palabra reservada";
 		} else if (token.matches("(true|false)")) { // bool
 			return "bool";
-		} else if (token.matches(especiales)) { // caracteres especiales
-			return "caracter especial";
 		} else if (token.matches(compuestos)) { // operadores compuestos
 			return "operador compuesto";
+		} else if (token.matches(especiales)) { // caracteres especiales
+			return "caracter especial";
 		} else if (token.matches("[a-z]([0-9]|[a-z]|[A-Z]|_)*")) { // identificadores
 			return "identificador";
 		} else if (token.matches("(-)?[0-9]+(\\.)[0-9]+")) { // float
